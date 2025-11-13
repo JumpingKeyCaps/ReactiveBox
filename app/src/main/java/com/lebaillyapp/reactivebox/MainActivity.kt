b79d82a6313a70a8ff566e00970b1eec7db930e3
Samuel Lebailly <sam.meusa@gmail.com>
2025-11-13 18:03:20 +0100

    feature(main/): Init the sensorViewmodel + add the sensorDebugView to the main composition screen.

--- app/src/main/java/com/lebaillyapp/reactivebox/MainActivity.kt
+++ app/src/main/java/com/lebaillyapp/reactivebox/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,16 +13,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.lebaillyapp.reactivebox.ui.composition.SensorDebugView
 import com.lebaillyapp.reactivebox.ui.theme.ReactiveBoxTheme
+import com.lebaillyapp.reactivebox.ui.viewModel.SensorViewModel
 
 class MainActivity : ComponentActivity() {
+
+
+    private val sensorViewModel: SensorViewModel by viewModels()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ReactiveBoxTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box( modifier = Modifier.fillMaxSize().padding(innerPadding) ){
 
+                        //debug sensor
+                        SensorDebugView(viewModel = sensorViewModel)
+
+                    }
                 }
             }
         }
